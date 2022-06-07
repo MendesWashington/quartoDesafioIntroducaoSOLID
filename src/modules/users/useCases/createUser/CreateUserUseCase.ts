4dae8b0cbf811329c006ad1f52eeae1fab76589d
Washington Mendes <washington.mendes@zigpay.com.br>
2022-06-07 02:13:19 -0300

    commitanto os primeiro ajustes dos testes

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -7,10 +8,19 @@ interface IRequest {
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ email, name }: IRequest): User {
     // Complete aqui
+    const user = this.usersRepository.findByEmail(email);
+    if (user) {
+      throw new Error("User already registered!")
+    }
+    const newUser = this.usersRepository.create({ name, email });
+    if (newUser) {
+      return newUser
+    }
+    throw new Error("Salve user failed!")
   }
 }
 
